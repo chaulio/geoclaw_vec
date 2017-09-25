5b0c2199391afbc53d058dbf36761d48b70ad761
Chaulio Ferreira <chaulio@hotmail.com>
2017-09-25 23:52:26 +0200

    Fixed bug in SuperMIC/submit_all.sh

--- SuperMIC/submit_all.sh
+++ SuperMIC/submit_all.sh
@@ -2,9 +2,9 @@
 for machine in host mic
 do
 	if [ "$machine" = "mic" ] ; then
-		num_threads_list=001 002 004 008 015 030 060 120
+		num_threads_list="001 002 004 008 015 030 060 120"
 	else
-		num_threads_list=01 02 04 08 16 32
+		num_threads_list="01 02 04 08 16 32"
 	fi
 
 	for version in 1 2
