76126002ef565fca83e68bcc1cef017c9296c44f
Chaulio Ferreira <chaulio@hotmail.com>
2017-09-25 22:55:00 +0200

    Improved SuperMIC scripts and added #if defined (_OPENMP) around ! SIMD directives. If OpenMP is not available, standard !DIR SIMD is used

--- SuperMIC/submit_all.sh
+++ SuperMIC/submit_all.sh
@@ -1,13 +1,24 @@
-for machine in mic host ; do
-	for version in 1 2 ; do
-        for openmp in no ; do 
-	
-            export machine
-            export version
-            export openmp
-            
-            echo "==> Submitting: $machine $version, openmp: $openmp"
-            llsubmit /home/hpc/pr45fi/di49rew/geoclaw_vec/SuperMIC/execute.sh
-         done;
+
+for machine in host mic
+do
+	if [ "$machine" = "mic" ] ; then
+		num_threads_list=001 002 004 008 015 030 060 120
+	else
+		num_threads_list=01 02 04 08 16 32
+	fi
+
+	for version in 1 2
+	do
+		for num_threads in $num_threads_list
+		do
+			export machine
+			export version
+			export num_threads
+		        echo "==> Submitting: $machine v$version, threads: $num_threads"
+		        llsubmit /home/hpc/pr45fi/di49rew/geoclaw_vec/SuperMIC/job.sh
+		done;
 	done;
 done;
+
+
+
